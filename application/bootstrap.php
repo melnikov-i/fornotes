dc39aacdc21f9d415cc4e65b60cc9b86410d070c
Ivan Melnikov <melnikov-ivan@mail.ru>
2017-11-08 19:43:06 +0000

    BackEnd : Bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -18,7 +18,8 @@
 
     function router() {
       $controllerName = 'main';
-      $actionName = 'index';      
+      $actionType = 'index';
+      $actionPayload = '';
       /* Обработка HTTP-запроса */
       $request = $_SERVER['REQUEST_URI'];
       $splits = explode('/', trim($request, '/'));
@@ -30,16 +31,18 @@
       if ( !empty($splits[$this->index + 1]) ) {
         $actionName = mb_strtolower($splits[$this->index + 1]);
       }
-      /* Имя файла, в котором описан контроллер */
-      $controllerFile = $controllerName.'.php';
-      /* Путь к файлу, в котором описан контроллер */
-      $controllerPath = __CONTROLLERS__.$controllerFile;
-      if ( file_exists($controllerPath) ) {
-        require_once $controllerPath;
-      } else {
-        header('HTTP/1.1 404 Not Found');
-        header('Status: 404 Not Found');
-        require_once __ERROR_PAGE__;
-      }
+      /* Получаем данные экшена */
+
+      // /* Имя файла, в котором описан контроллер */
+      // $controllerFile = $controllerName.'.php';
+      // /* Путь к файлу, в котором описан контроллер */
+      // $controllerPath = __CONTROLLERS__.$controllerFile;
+      // if ( file_exists($controllerPath) ) {
+      //   require_once $controllerPath;
+      // } else {
+      //   header('HTTP/1.1 404 Not Found');
+      //   header('Status: 404 Not Found');
+      //   require_once __ERROR_PAGE__;
+      // }
     }
   }
